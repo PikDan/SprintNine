41c97dd139621bc925e3827c049d54b5cf41a725
Daniil Dushkov <dendidany@gmail.com>
2026-03-11 16:34:24 +0300

    created multithreaded-maxvalue-search

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"slices"
+	"sync"
+	"time"
 )
 
 const (
@@ -10,31 +14,95 @@ const (
 )
 
 // generateRandomElements generates random elements.
-func generateRandomElements(size int) []int {
+func generateRandomElements(size int) ([]int, error) {
 	// ваш код здесь
+	if size <= 0 {
+		return nil, fmt.Errorf("Размер должен быть > 0")
+	}
+	data := make([]int, size)
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for i := 0; i < size; i++ {
+
+		data[i] = r.Intn(9999)
+	}
+	return data, nil
 }
 
 // maximum returns the maximum number of elements.
-func maximum(data []int) int {
+func maximum(data []int) (int, error) {
 	// ваш код здесь
+	if len(data) < 1 {
+		return 0, fmt.Errorf("Размер должен быть > 0")
+	}
+	maxNumber := slices.Max(data)
+	return maxNumber, nil
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
-func maxChunks(data []int) int {
+func maxChunks(data []int) (int, error) {
 	// ваш код здесь
+	if len(data) < CHUNKS {
+		return 0, fmt.Errorf("Размер должен быть > числа чанков")
+	}
+	var wg sync.WaitGroup
+	totalMaxVals := make([]int, CHUNKS)
+
+	chopData := len(data) / CHUNKS
+
+	for i := 0; i < CHUNKS; i++ {
+
+		firstIndx := i * chopData
+		lastIndx := firstIndx + chopData
+
+		if i == CHUNKS-1 {
+			lastIndx = len(data)
+		}
+
+		wg.Add(1)
+
+		go func(i, firstIndx, lastIndx int) {
+			defer wg.Done()
+
+			localMaxNum := slices.Max(data[firstIndx:lastIndx])
+			totalMaxVals[i] = localMaxNum
+		}(i, firstIndx, lastIndx)
+
+	}
+
+	wg.Wait()
+
+	return slices.Max(totalMaxVals), nil
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел ", SIZE)
 	// ваш код здесь
+	genetatedData, err := generateRandomElements(SIZE)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println("Ищем максимальное значение в один поток")
+	fmt.Println("Ищем максимальное значение в один поток ")
 	// ваш код здесь
+	start := time.Now()
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	max, err := maximum(genetatedData)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	elapsed := time.Since(start)
+
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed.Microseconds())
+
+	fmt.Printf("Ищем максимальное значение в %d потоков ", CHUNKS)
 	// ваш код здесь
+	start = time.Now()
+	max, err = maxChunks(genetatedData)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	elapsed = time.Since(start)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed.Microseconds())
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,98 @@
 package main
 
 // Пишите тесты в этом файле
+import (
+	"slices"
+	"testing"
+)
+
+func TestGenerateRandomElements(t *testing.T) {
+
+	data, err := generateRandomElements(10)
+
+	if err != nil {
+		t.Fatalf("ошибка генерации 10 элементов: %v", err)
+	}
+
+	if len(data) != 10 {
+		t.Fatalf("Ожидалась длина 10, получено %d", len(data))
+	}
+}
+
+func TestGenerateRandomElementsError(t *testing.T) {
+
+	_, err := generateRandomElements(0)
+
+	if err == nil {
+		t.Fatalf("Ожидалась ошибка, но получено значение nil")
+	}
+}
+
+func TestMaxChunksLessThanChunks(t *testing.T) {
+
+	data := []int{1, 5, 3}
+
+	_, err := maxChunks(data)
+	if err == nil {
+		t.Fatalf("ожидалась ошибка при len(data) < CHUNKS: %v", err)
+	}
+
+}
+
+func TestMaximum(t *testing.T) {
+
+	data := []int{1, 5, 3, 9, 2, 11, 66, 77, 88}
+
+	max, err := maximum(data)
+
+	if err != nil {
+		t.Fatalf("неожиданная ошибка поиска max: %v", err)
+	}
+
+	if max != 88 {
+		t.Fatalf("Ожидалось 88, получено: %d", max)
+	}
+}
+func TestMaximumEmptySlice(t *testing.T) {
+
+	_, err := maximum([]int{})
+
+	if err == nil {
+		t.Fatalf("Ожидалась ошибка, но получено значение nil")
+	}
+}
+func TestMaxChunks(t *testing.T) {
+
+	data := []int{1, 5, 3, 9, 2, 11, 4, 99, 22, 11}
+
+	expected := slices.Max(data)
+
+	result, err := maxChunks(data)
+	if err != nil {
+		t.Fatalf("неожиданная ошибка поиска maxChunks: %v", err)
+	}
+	if result != expected {
+		t.Fatalf("Ожидалось %d, получено %d", expected, result)
+	}
+}
+func TestMaxChunksLarge(t *testing.T) {
+
+	data, err := generateRandomElements(10000)
+
+	if err != nil {
+		t.Fatalf("ошибка генерации 10000 элементов: %v", err)
+	}
+
+	expected, err := maximum(data)
+	if err != nil {
+		t.Fatalf("неожиданная ошибка поиска max: %v", err)
+	}
+	result, err := maxChunks(data)
+	if err != nil {
+		t.Fatalf("неожиданная ошибка поиска maxChunks: %v", err)
+	}
+
+	if result != expected {
+		t.Fatalf("Ожидалось %d, получено %d", expected, result)
+	}
+}
